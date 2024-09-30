a01a7e048533371551d3017523e20dd5ee7f5ef4
Luiz Gustavo Neckel <luiz.neckel@estudante.ifms.edu.br>
2024-09-30 14:13:52 +0000

    aula 30-09

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Footer from "./modulos/footer";
 import Navbar from "./modulos/navbar";
 
@@ -5,6 +8,19 @@ export default function Index() {
   return <div>
     <Navbar />
     Bem Vindos
+    <Container>
+      <Row>
+        <Col className='bg-info-subtle'>1 of 2</Col>
+        <Col className='bg-black'>2 of 2</Col>
+      </Row>
+      <Row>
+        <Col className='bg-primary'>1 of 3</Col>
+        <Col className='bg-success'>2 of 3</Col>
+        <Col className='bg-warning'>3 of 3</Col>
+      </Row>
+    </Container>
     <Footer />
   </div>;
 }
+
+
