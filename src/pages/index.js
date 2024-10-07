63788b0fcacdc63b85ddb3b0858aaa75fd5c4b3e
Luiz Gustavo Neckel <luiz.neckel@estudante.ifms.edu.br>
2024-10-07 14:15:14 +0000

    aula 07-10

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,12 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import {Container, Row, Col} from 'react-bootstrap';
 import Footer from "./modulos/footer";
-import Navbar from "./modulos/navbar";
+import Menu from "./modulos/navbar";
 
 export default function Index() {
   return <div>
-    <Navbar />
-    Bem Vindos
-    <Container>
-      <Row>
-        <Col className='bg-info-subtle'>1 of 2</Col>
-        <Col className='bg-black'>2 of 2</Col>
-      </Row>
-      <Row>
-        <Col className='bg-primary'>1 of 3</Col>
-        <Col className='bg-success'>2 of 3</Col>
-        <Col className='bg-warning'>3 of 3</Col>
-      </Row>
-    </Container>
+    <Menu />
+    Bem Vindos a disciplina
+   
     <Footer />
   </div>;
 }
--- src/pages/modulos/navbar.js
+++ src/pages/modulos/navbar.js
@@ -1,4 +1,18 @@
-function Navbar() {
-    return<div>Menu FW</div>    
+import {Container, Nav, Navbar} from 'react-bootstrap';
+export default function Menu() {
+  return (
+    <Navbar expand="sm" className="bg-success">
+      <Container>
+        <Navbar.Brand href="#home">6A</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Produtos</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
-export default Navbar;
+
