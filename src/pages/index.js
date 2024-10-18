a2cf9783635b518d75eb400599acb75caba992b0
Luiz Gustavo Neckel <luiz.neckel@estudante.ifms.edu.br>
2024-10-18 11:17:57 +0000

    aula 14-10

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,12 @@
-import {Container, Row, Col} from 'react-bootstrap';
 import Footer from "./modulos/footer";
 import Menu from "./modulos/navbar";
+import { Container, Row, Col } from 'react-bootstrap';
+
 
 export default function Index() {
   return <div>
     <Menu />
     Bem Vindos a disciplina
-   
     <Footer />
   </div>;
 }
-
-
