dda5c7423ce6a0906f6d2899fe3499fff53eb866
Luiz Gustavo Neckel <luiz.neckel@estudante.ifms.edu.br>
2024-10-21 12:45:11 +0000

    aula 18-10

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,9 @@ import { Container, Row, Col } from 'react-bootstrap';
 export default function Index() {
   return <div>
     <Menu />
+    <Container className=".bg-success.bg-gradient">
     Bem Vindos a disciplina
+    </Container>
     <Footer />
   </div>;
 }
--- src/pages/modulos/navbar.js
+++ src/pages/modulos/navbar.js
@@ -1,9 +1,9 @@
 import { Container, Nav, Navbar } from 'react-bootstrap';
-import styles from "@/styles/Navbar.module.css"
+import styles from "@/styles/navbar.module.css"
 export default function Menu() {
   return (
     <Container>
-      <Navbar expand="sm" className={` bg-dark ${styles.navGradiente} navbar-dark px-3 m-0`}>
+      <Navbar expand="sm" className={`${styles.navGradiente}  text-dark px-3 m-0 border border-info-subtle rounded rounded-4`}>
         <Navbar.Brand href="/">6A</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
